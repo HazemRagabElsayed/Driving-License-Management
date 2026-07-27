fb15b034091b567bfd9c265ce26e7efd54058beb
HazemRagabElsayed <hazem1750@gmail.com>
2026-07-27 18:58:04 +0300

    Added Guid ,directory creation and savingimage function to use it for people images

--- DVLD-Presentation-Layer/Global Classes/clsUtil.cs
+++ DVLD-Presentation-Layer/Global Classes/clsUtil.cs
@@ -5,19 +5,80 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Drawing;
+using System.IO;
+using System.Linq.Expressions;
 
 namespace MySolution.Global_Classes
 {
     public class clsUtil
     {
+
+        static public string GenerateGuid()
+        {
+            Guid NewGuid = Guid.NewGuid();
+
+            return NewGuid.ToString();
+        }
+
+        static public string GenerateGuidWithExtensionOfFile(string FileName)
+        {
+            return GenerateGuid() + (new FileInfo(FileName).Extension);
+        }
+
+        static public bool CreateFolderIfDoesntExist(string FolderPath)
+        {
+            if (!Directory.Exists(FolderPath))
+            {
+                try
+                {
+                    Directory.CreateDirectory(FolderPath);
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    {
+                        MessageBox.Show("Error Creating Folder :" + ex.Message);
+                        return false;
+                    }
+
+                }
+            }
+            return true;
+        }
+
+        static public bool CopyImageToProjectFolder(ref string SourceFile)
+        {
+            if (!CreateFolderIfDoesntExist(@"D:\Repos\Driving-License-Management\DVLD-People-Images"))
+            {
+                return false;
+            }
+
+            string DestFile = @"D:\Repos\Driving-License-Management\DVLD-People-Images\" + GenerateGuidWithExtensionOfFile(SourceFile);
+
+            try
+            {
+                File.Copy(SourceFile, DestFile, true);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error saving image :" + ex.Message);
+                return false;
+            }
+
+            SourceFile = DestFile;
+
+            return true;
+        }
+
         static public void CenterLabelTitle(Size Size, Label lbl)
         {
 
 
-            int x = (Size.Width - lbl.Width) / 2;
-            int y = (Size.Height - lbl.Height) / 2;
+                int x = (Size.Width - lbl.Width) / 2;
+                int y = (Size.Height - lbl.Height) / 2;
 
-            lbl.Location = new Point(x, lbl.Location.Y);
+                lbl.Location = new Point(x, lbl.Location.Y);
         }
     }
 }
